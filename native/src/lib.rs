6545faf22f531e065689e29db42b71d44910766e
Amila Welihinda <amilajack@gmail.com>
2018-10-25 17:54:27 -0700

    initial commit

---
+++ native/src/lib.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate neon;
+#[macro_use]
+extern crate neon_serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    name: String,
+    age: u16,
+}
+
+export! {
+    fn say_hello(name: String) -> String {
+        format!("Hello, {}!", name)
+    }
+
+    fn greet(user: User) -> String {
+        format!("{} is {} years old", user.name, user.age)
+    }
+
+    fn fibonacci(n: i32) -> i32 {
+        match n {
+            1 | 2 => 1,
+            n => fibonacci(n - 1) + fibonacci(n - 2)
+        }
+    }
+}
