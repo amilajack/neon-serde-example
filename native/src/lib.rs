6604cbef3e3b84a42fd0ae53152f38ac183e3ee1
Amila Welihinda <amilajack@gmail.com>
2018-10-25 18:11:19 -0700

    remove unnecessary serde_json crate

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,8 +4,6 @@ extern crate neon;
 extern crate neon_serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
